ff56fb2b51f8e8169ce9d242774cc7351311228c
Seigo Osawa <guilhermeosawa@gmail.com>
2010-08-19 18:56:23 -0300

    List of projects shows # of pomos / expected_pomos

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,15 @@ class Project < ActiveRecord::Base
   validates_length_of :name, :in => 2..30, :allow_nil => false
   validates_uniqueness_of :name, :scope => :user_id, :case_sensitive => false
   
+  def count_pomos
+    self.tasks.count(:all, :joins => :pomodoros)
+  end
+=begin
+  def count_expected_pomos
+    self.tasks.each do |t|
+      count += t.expected_pomo
+    end
+    count
+  end
+=end
 end
