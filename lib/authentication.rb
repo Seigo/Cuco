cd307ad6791b2f9c97f5a031a84c16f447e30101
Fabiano PS <fabianosoriani@gmail.com>
2010-08-23 19:41:43 -0300

    minnor auth correction

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -21,7 +21,7 @@ module Authentication
   end
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= ((User.find(session[:user_id]) if session[:user_id]) rescue false)
   end
   
   def logged_in?
